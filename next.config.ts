39e390424a0b9fa7c8c35b12978b45f0835a426e
Mohamed Ibrahim Mohamed Isfak <isfak216@gmail.com>
2026-09-14 20:00:19 +0530

    Remove allowedDevOrigins from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.53.253.28", "localhost", "127.0.0.1"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
